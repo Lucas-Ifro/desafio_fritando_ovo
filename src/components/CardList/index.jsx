e4a30377398f9b994c23c99ffee6616d8c410a2c
lucas.ferreira <lucaslfr00@gmail.com>
2024-10-09 22:18:53 -0400

    aula do dia 9 de out

--- src/components/CardList/index.jsx
+++ src/components/CardList/index.jsx
@@ -5,22 +5,25 @@ import Cartao from '../Cartao'
 export default function CardList(){
 
     const [cards, setCards] = useState([
-        {id: 1, titulo: "ovo com arroz e pepino", tempo:10, serve:2},
-        {id: 2, titulo: "ovo com ovo", tempo:20, serve:1},
-        {id: 3, titulo: "ovo com pão", tempo:30, serve:4},
-        {id: 4, titulo: "ovo com mexido", tempo:5, serve:1}
+        {id: 1, titulo: "ovo com arroz e pepino", tempo:10, serve:2 ,imagem:"ovo_pepino.png"},
+        {id: 2, titulo: "ovo com ovo",            tempo:20, serve:1 ,imagem:"ovo_pepino.png"},
+        {id: 3, titulo: "ovo com pão",            tempo:30, serve:4 ,imagem:"ovo_pepino.png"},
+        {id: 4, titulo: "ovo com mexido",         tempo:5,  serve:1 ,imagem:"ovo_pepino.png"}
     ])
 
     return(
         <div className='flex flex-wrap flex-col m-8'>
             <div>Quantidade de Receitas: {cards.length}</div>
-            <ul className='list-disc'>
-                {
-                    cards.map(c => (
-                        <li>{c.titulo} serve {c.serve} pessoa(s).</li>
-                    ))
-                }
-            </ul>
+            <div className='flex flex-wrap'>
+                {cards.map(c =>(
+                    <Cartao
+                    imagem = {c.imagem}
+                    titulo = {c.titulo}
+                    tempo = {c.tempo}
+                    serve = {c.serve}
+                    />
+                ))}
+            </div>
         </div>
     )
 }
--- src/components/Cartao/index.jsx
+++ src/components/Cartao/index.jsx
@@ -1,20 +1,20 @@
 import { Clock, Soup } from "lucide-react";
 import Image from "next/image";
 
-export default function Cartao() {
+export default function Cartao({imagem, titulo, tempo, serve}) {
     return(
         <div className="m-4 rounded border-2 border-solid border-rose-400 max-w-[387px]">
-            <Image className="bg-cover bg-center" src="/assets/images/ovo_pepino.png" width={387} height={297} alt="Prato de ovo com pepino"/>
+            <Image className="bg-cover bg-center" src={`/${imagem}`} width={387} height={297} alt={titulo}/>
             <h2 className="p-2 bg-rose-300 text-gray-800">
-                Ovo com arroz e pepino
+                {titulo}
             </h2>
             <div className="p-2 text-xs flex items-center gap-2">
                 <Clock />
-                25 minutos
+                {tempo} minuto(s)
             </div>
             <div className="p-2 text-xs flex items-center gap-2">
                 <Soup />
-                serve 2 pessoas
+                serve {serve} pessoa(s)
             </div>
         </div>
     ) 
